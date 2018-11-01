268f1fccdd246f0f3f59ff21f5afae32720d643b
hicha <hicha@DESKTOP-FBLD947>
2018-11-01 19:41:29 +0100

    security up

--- SpringMvc/src/main/java/org/m2i/security/SecurityConfiguration.java
+++ SpringMvc/src/main/java/org/m2i/security/SecurityConfiguration.java
@@ -1,5 +1,25 @@
 package org.m2i.security;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//est une class de configuration donc il faut le annoter @configuration
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.inMemoryAuthentication().withUser("admin").password("hicham").roles("USER","ADMIN");
+	auth.inMemoryAuthentication().withUser("user").password("hicham").roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.authorizeRequests().antMatchers("/index").hasRole("USER");
+	    http.authorizeRequests().antMatchers("/form","/save","edit","/delete").hasRole("ADMIN");
+	}
 }
